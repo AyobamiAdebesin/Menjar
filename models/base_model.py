4ed6d696a7f066cacd0a0c397ceff2c8a0edc756
AyobamiAdebesin <froshofficial@gmail.com>
2022-11-19 09:24:56 +0100

    Implemented storage method in basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -28,6 +28,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """ String representation of the BaseModel """
@@ -39,6 +40,7 @@ class BaseModel:
         """ Updated the public instance attribute updated_at
         with the current datetime """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
